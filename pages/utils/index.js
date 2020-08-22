8c1b0e712aa4ed7928848c05dc20fec40187c3d0
Ebuks <ebuks@Ebuks-Mac.local>
2020-08-22 13:08:27 +0100

    commented out dynamic import in pages/utils/index.js

--- pages/utils/index.js
+++ pages/utils/index.js
@@ -1,4 +1,4 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import { Footer } from "./footer";
 import { useMenuStore } from "./menu/utils";
 import { useApp } from "../_app.page";
